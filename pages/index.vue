ea03ad3ee37423c3abfa7dfe849dfde82ab925cc
Alberto F Alfredo <albertoalfredo.profissional@gmail.com>
2023-02-21 03:00:48 -0300

    Primeiro teste de deploy automatico com github actions para hsotgator

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <img class="banner-img" style="width: 10rem" src="~/assets/img/logo-branco.png">
     </div>
     <div>
-      <h1>Welcome to the homepage</h1>
+      <h1>São 03:00 da madrugada e agora que consegui fazer funcionar, eu estava correto desde o primeiro teste mas eu estava usando o githubpages para testes e ele não roda legal, ao subir a primeira versão na hostgator funcionou, vou deixar isso aqui para nunca esquecer</h1>
     </div>
   </div>
 </template>
