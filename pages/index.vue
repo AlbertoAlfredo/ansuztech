f3b24b708e0680e2b783beaf4b10765af0fbdd11
Alberto F Alfredo <albertoalfredo.profissional@gmail.com>
2023-02-21 02:26:50 -0300

    Tentativa de fundionar as imagens no gh-pages

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="banner efeito-verde">
+      <img class="banner-img" style="width: 10rem" src="~/assets/img/logo-branco.png">
+    </div>
+    <div>
+      <h1>Welcome to the homepage</h1>
+    </div>
+  </div>
+</template>
+<style lang="css" scoped>
+  .banner{
+      height: 100%;
+      border: 2px #38383800 solid;
+      border-radius: 10px;
+      padding: 1rem;
+      transition: border 2s;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      margin-bottom: 4rem;
+  }
+  .banner:hover{
+      border-color: #fff ;
+  }
+  .banner-img{
+      margin: 2rem;
+      margin-right: 6rem;
+  }
+</style>
+<style>
+@import url("~/assets/styles/reset.css");
+@import url("~/assets/styles/style.css");
+</style>
