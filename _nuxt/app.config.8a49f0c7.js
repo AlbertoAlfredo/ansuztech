19b1ed215e3245d88e1fd8ced376c2400664e52f
Alberto F Alfredo <albertoalfredo.profissional@gmail.com>
2023-02-21 02:27:11 -0300

    Updates

---
+++ _nuxt/app.config.8a49f0c7.js
@@ -0,0 +1,1 @@
+import{i as u,A as s}from"./entry.674da645.js";function t(e,n){return u()._useHead(e,n)}const i={};s(i);export{t as u};
---
+++ _payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1676957228069}
