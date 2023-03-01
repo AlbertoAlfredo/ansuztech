25c367082313452c395ca112fd30c5854b48f380
Alberto F Alfredo <albertoalfredo.profissional@gmail.com>
2023-03-01 14:11:42 -0300

    Resolvido problema das imagens, no vue não consegui colocar de forma dinâmica, quando tiver imagens terei que colocar de forma mais manual

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="banner">
+        <div class="banner-text">
+        <h1>ANSUZ TECH</h1>
+        <h2>A solução para sua empresa</h2>
+        </div>
       <img class="banner-img" style="width: 10rem" src="~/assets/img/logo-branco.png">
     </div>
 </template>
@@ -9,18 +13,26 @@
       border: 2px #38383800 solid;
       border-radius: 10px;
       padding: 1rem;
-      transition: border 2s;
+      transition: 2s;
       display: flex;
-      flex-direction: column;
-      align-items: flex-end;
+      flex-direction: row;
+      font-family: monospace;
       margin: 1%;
       margin-bottom: 4rem;
+      align-items: center;
+      justify-content: space-between;
+      color: #ffffff00;
   }
   .banner:hover{
       border-color: #fff ;
+      color: #fff !important;
   }
   .banner-img{
       margin: 2rem;
       margin-right: 6rem;
+  }
+  .banner-text{
+    
+    font-size: 4rem;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -8,14 +8,49 @@
       <h1>São 03:00 da madrugada e agora que consegui fazer funcionar, eu estava correto desde o primeiro teste mas eu estava usando o githubpages para testes e ele não roda legal, ao subir a primeira versão na hostgator funcionou, vou deixar isso aqui para nunca esquecer</h1>
     </div> -->
       </div>
-      <div v-for="card in cards">
+      <div>
         <Card 
-        :title="card.title"
-        :content="card.content"
-        :img="card.img"
-        :invert="card.invert"
+        title="Crie uma presença online impressionante"
+        content="Com o design certo, você pode criar uma presença online impressionante que destaca sua marca. Seja visto e ouvido em um mercado digital competitivo. Deixe-nos ajudá-lo a criar uma presença online que inspira e atrai seus clientes ideais."
+        invert="invert efeito-amarelo"
        >
-       <img :class="'card-img ' + invert" :src="`/img/${card.img}`" />
+       <img class="card-img invert" src="~assets/img/1.jpg" />
+       </Card>
+      </div>
+      <div>
+        <Card 
+        title="'Design que destaca sua marca em meio à concorrência'"
+          content='Em um mercado digital saturado, o design é fundamental para destacar sua marca. Nós criamos designs exclusivos e inovadores que se destacam da multidão. Não deixe sua marca se perder na concorrência - confie em nós para criar um design que a coloca à frente.'
+          invert='efeito-verde'
+       >
+       <img class="card-img invert" src="~assets/img/2.jpg" />
+       </Card>
+      </div>
+      <div>
+        <Card 
+        title='Transforme sua ideia em um site incrível'
+          content='Sua ideia merece ser vista em toda a sua glória. Deixe-nos transformá-la em um site incrível que irá encantar e envolver seus visitantes. Nós oferecemos design personalizado e soluções técnicas para ajudá-lo a alcançar seus objetivos online.'
+          invert='invert efeito-laranja'
+       >
+       <img class="card-img invert" src="~assets/img/3.jpg" />
+       </Card>
+      </div>
+      <div>
+        <Card 
+        title='Design elegante, funcionalidade impecável'
+          content='Nosso foco é combinar um design elegante com uma funcionalidade impecável para criar uma experiência do usuário excepcional. Oferecemos soluções de design personalizadas que se adaptam às necessidades de sua marca. Confie em nós para criar um design que impressiona visualmente e funciona perfeitamente.'
+          invert='efeito-vermelho'
+       >
+       <img class="card-img invert" src="~assets/img/4.jpg" />
+       </Card>
+      </div>
+      <div>
+        <Card 
+        title='Sites que são fáceis de usar e impressionam visualmente'
+          content='Nós acreditamos que um site deve ser tão fácil de usar quanto é visualmente atraente. É por isso que criamos sites que oferecem a combinação perfeita de beleza e funcionalidade. Deixe-nos ajudá-lo a criar um site que faça sua marca se destacar, enquanto oferece uma experiência de usuário excepcional.'
+          invert='invert efeito-amarelo'
+       >
+       <img class="card-img invert" src="~assets/img/5.jpg" />
        </Card>
       </div>
     </main>
@@ -26,38 +61,7 @@
 export default {
   data(){
     return {
-      cards:[
-        {
-          title: 'Crie uma presença online impressionante',
-          content: 'Com o design certo, você pode criar uma presença online impressionante que destaca sua marca. Seja visto e ouvido em um mercado digital competitivo. Deixe-nos ajudá-lo a criar uma presença online que inspira e atrai seus clientes ideais.',
-          img:'1.jpg',
-          invert:'invert efeito-amarelo'
-        },
-        {
-          title: 'Design que destaca sua marca em meio à concorrência',
-          content: 'Em um mercado digital saturado, o design é fundamental para destacar sua marca. Nós criamos designs exclusivos e inovadores que se destacam da multidão. Não deixe sua marca se perder na concorrência - confie em nós para criar um design que a coloca à frente.',
-          img:'2.jpg',
-          invert:'efeito-verde'
-        },
-        {
-          title: 'Transforme sua ideia em um site incrível',
-          content: 'Sua ideia merece ser vista em toda a sua glória. Deixe-nos transformá-la em um site incrível que irá encantar e envolver seus visitantes. Nós oferecemos design personalizado e soluções técnicas para ajudá-lo a alcançar seus objetivos online.',
-          img:'3.jpg',
-          invert:'invert efeito-laranja'
-        },
-        {
-          title: 'Design elegante, funcionalidade impecável',
-          content: 'Nosso foco é combinar um design elegante com uma funcionalidade impecável para criar uma experiência do usuário excepcional. Oferecemos soluções de design personalizadas que se adaptam às necessidades de sua marca. Confie em nós para criar um design que impressiona visualmente e funciona perfeitamente.',
-          img:'4.jpg',
-          invert:'efeito-vermelho'
-        },
-        {
-          title: 'Sites que são fáceis de usar e impressionam visualmente',
-          content: 'Nós acreditamos que um site deve ser tão fácil de usar quanto é visualmente atraente. É por isso que criamos sites que oferecem a combinação perfeita de beleza e funcionalidade. Deixe-nos ajudá-lo a criar um site que faça sua marca se destacar, enquanto oferece uma experiência de usuário excepcional.',
-          img:'5.jpg',
-          invert:'invert efeito-amarelo'
-        },
-      ]
+
     }
   }
 }
@@ -66,8 +70,7 @@ export default {
 <style lang="css" scoped>
     .card-img{
       margin: 2rem;
-      margin-right: 6rem;
-      width: 5rem;
+      width: 20rem;
   }
 </style>
 <style>
